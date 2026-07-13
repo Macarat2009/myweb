b8a6805e6a773ddd61f49f9b59a067a4790e8d33
Macarat2009 <mat.becvar@gmail.com>
2026-07-13 21:07:23 +0200

    1

---
+++ script.js
@@ -0,0 +1,17 @@
+const buttons = document.querySelectorAll(".nav-item");
+
+
+buttons.forEach(button => {
+
+    button.addEventListener("click",()=>{
+
+        buttons.forEach(btn=>{
+            btn.classList.remove("active");
+        });
+
+
+        button.classList.add("active");
+
+    });
+
+});
